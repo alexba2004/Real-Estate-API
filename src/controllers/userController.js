5a9fa169185a699dafe17c9275cf975c054d1160
Briones <j.alejandro.briones.a22@gmail.com>
2023-10-18 09:48:15 -0600

    Commit with some changes in views and controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import { request, response } from "express";
 import User from "../models/User.js";
+import { check, validationResult } from "express-validator";
 
 const formLogin = (req, res) => {
     res.render("auth/login.pug", { isLoged: false });
@@ -18,8 +19,26 @@ const formPasswordRecovery = (request, response) => {
 };
 
 const insertUser = async (request, response) => {
-    console.log(`===============[DATA]================\nIntentando registrar los datos del usuario en la Base de datos\nNombre: ${request.body.name}\n-------------------------------------`);
-    let newUser = await User.create(request.body);
+    console.log(`===============[DATA]================\nIntentando registrar los datos del usuario en la Base de datos\nNombre: ${request.body.name}\nPassword: ${request.body.password}\n-------------------------------------`);
+
+    await check("name").notEmpty().withMessage("This Field is required").run(request);
+    await check("email").notEmpty().withMessage("This Field is required").isEmail().withMessage("This is not in email format").run(request);
+    await check("password").notEmpty().withMessage("This Field is required").isLength({ min: 8 }).withMessage("The password must have 8 characters at least").run(request);
+    await check("confirmPassword").notEmpty().withMessage("This Field is required").isLength({ min: 8 }).withMessage("The password must have 8 characters at least").equals(request.body.password).withMessage("Both passwords field must be the same").run(request);
+
+    console.log(`Se encontraron: ${validationResult.length} errores de validación`);
+    // response.json(validationResult(request));
+
+    let validation = validationResult(request);
+
+    if (validation.isEmpty()) {
+        let newUser = await User.create(request.body);
+    } else {
+        response.render("auth/register.pug", {
+            page: "New Account",
+            errors: validation.array(),
+        });
+    }
 };
 
 export { formLogin, formRegister, formPasswordRecovery, insertUser };
