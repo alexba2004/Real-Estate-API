046f42e0de68a7fb3a7070295eae2917fe143c30
Alejandro BA <122334857+alexba2004@users.noreply.github.com>
2023-11-18 20:32:01 -0600

    Delete directories

--- src/index.js
+++
@@ -1,2 +0,0 @@
-// Código JS
-console.log("Hola Mundo desde JS a tráves de Express, modificado mientras esta corriendo nodemon");
--- src/lif/tokens.js
+++
@@ -1,9 +0,0 @@
-const generateToken = () => {
-    const random1 = Math.random().toString(32).substring(2);
-    const timestamp = Date.now().toString(32);
-    const random2 = Math.random().toString(32).substring(2);
-
-    return random1 + timestamp + random2;
-};
-
-export default generateToken;
