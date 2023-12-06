c6ba067fa3829b72b66fa8b8277a38404bd91873
Alejandro BA <122334857+alexba2004@users.noreply.github.com>
2023-12-06 12:49:10 -0600

    fix/delete: some changes in the project

--- src/public/js/setTime.js
+++
@@ -1,6 +0,0 @@
-setTimeout(() => {
-    const mensaje = document.getElementById("message");
-    if (mensaje) {
-        mensaje.style.display = "none";
-    }
-}, 4000);
--- src/routes/propertyRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { formProperty, saveNewPropery } from "../controllers/propertyController.js";
-const router = express.Router();
-
-router.get("/create", formProperty);
-router.post("/create", saveNewPropery);
-
-export default router;
